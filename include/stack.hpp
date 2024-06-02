663a5feb103a222a7a3fd1c4ed17f77655e48c20
David <doladepo128@gmail.com>
2024-06-02 11:31:55 -0700

    Added Stack data Structure

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,7 +1,30 @@
+#ifndef STACK_HPP
+#define STACK_HPP
 
+template <typename T>
+class Stack {
+    private:
+         struct Node {
+            T data;
+            Node* next;
+            Node(T val) : data(val), next(nullptr) {}
+         };
+         Node* top;
 
+         public:
+             Stack() : top(nullptr) {}
+             ~Stack();
 
+             void push(T val);
+             void pop();
+             T peek();
+             bool isEmpty();
+             void print();
+};
 
+#include "stack.cpp"
+
+#endif
 
 
 
--- src/stack.cpp
+++ src/stack.cpp
@@ -1,9 +1,51 @@
+#include "stack.hpp"
+#include <iostream>
+
+template <typename T>
+Stack<T>::~Stack() {
+    while (top != nullptr){
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+}
+
+template <typename T>
+void Stack<T>::push(T val){
+    Node* newNode = new Node(val);
+    newNode->next = top;
+    top = newNode;
+}
+
+template <typename T>
+void Stack<T>::pop() {
+    if (top == nullptr) return;
+    Node* temp = top;
+    top = top->next;
+    delete temp;
+}
+
+template <typename T>
+T Stack<T>::peek() {
+    if (top == nullptr) throw std::runtime_error("Stack is empty");
+    return top->data;
+}
 
 
+template <typename T>
+bool Stack<T>::isEmpty() {
+    return top == nullptr;
+}
 
-
-
-
+template <typename T>
+void Stack<T>::print() {
+    Node* current = top;
+    while(current != nullptr){
+        std::cout << current->data << " -> ";
+        current = current->next;
+    }
+    std:cout << "nullptr" << std::endl;
+}
 
 
 
