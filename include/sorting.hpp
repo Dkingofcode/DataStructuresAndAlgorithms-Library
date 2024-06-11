684389895b3f31f71cd58ac9551d0a2ec2b7663e
David <doladepo128@gmail.com>
2024-06-10 22:02:19 -0700

    finished insertion sort

--- include/sorting.hpp
+++ include/sorting.hpp
@@ -1,5 +1,16 @@
+#ifndef SORTING_HPP
+#define SORTING_HPP
 
+#include <vector>
 
+class Sorting {
+    public: 
+      static void bubbleSort(std::vector<int>& arr);
+};
+
+#include "sorting.cpp"
+
+#endif
 
 
 
--- src/sorting.cpp
+++ src/sorting.cpp
@@ -10,3 +10,51 @@
 //         }
 //     }
 // }
+
+#include "sorting.hpp"
+
+void Sorting::bubbleSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for(int i = 0; i < n - 1; ++i){
+        for(int j = 0; j < n - 1 - i; ++j){
+            if(arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void Sorting::insertionSort(std::vector<int>& arr){
+    int n = arr.size();
+    int i, key, j;
+    for(i = 1; i < n; i++){
+        key = arr[i];
+        j = i - 1;
+
+        // Move elements of arr[0...i - 1],
+        // that are greater than key,
+        // to one position ahead of their 
+        // current position
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    } 
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
