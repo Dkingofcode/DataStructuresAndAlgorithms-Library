6656314ca4ac7b2a7c4f2a9a1d0d7feadef402b6
David <doladepo128@gmail.com>
2024-06-22 18:54:55 -0700

    Added BFS, DFS and Graph algorithms

--- src/BFS.cpp
+++ src/BFS.cpp
@@ -14,6 +14,79 @@
 #include <map>
 #include <queue>
 #include <set>
+#include <stack>
+
+    std::vector<std::vector<int>> BreadthFirstSearch(TreeNode* root){
+       
+       std::vector<std::vector<int>> result;
+       if(root == nullptr){
+          return result;
+       };
+
+       std::queue<TreeNode*> q;
+        //queue<TreeNode*> q;
+           q.push({root});
+        while(!q.empty()){
+            int size = q.size();             
+            TreeNode* node = q.front();
+            q.pop();
+            std::vector<int> row;
+            
+            for(int i = 0; i < size; i++){
+                row.push_back(node->val);
+
+                if(node->left != nullptr){
+                    q.push(node->left);
+                }
+
+                if(node->right != nullptr){
+                    q.push(node->right);
+                }
+                
+            }
+            result.push_back(row);
+        }
+
+        return result;
+
+
+    }
+
+
+
+    std::vector<std::vector<int>> DepthFirstSearch(TreeNode* root) {
+           std::vector<std::vector<int>> result;
+       if(root == nullptr){
+          return result;
+       };
+
+       std::stack<std::pair<TreeNode*, int>> st;
+
+        //queue<TreeNode*> q;
+           st.push({root, 0});
+
+        while(!st.empty()){             
+            auto [node, depth] = st.top();
+            st.pop();
+            
+                if (depth == result.size()){
+                    result.push_back({});
+                }
+                result[depth].push_back(node->val);
+
+                if(node->right != nullptr){
+                    st.push({node->right, depth + 1});
+                }
+
+                if(node->left != nullptr){
+                    st.push({node->left, depth + 1});
+                }
+            }
+            
+
+        return result;
+
+    }
 
 
 
